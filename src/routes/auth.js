3c296d0cf2a6e598e52dff713addc3e14f76b63c
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-03-25 00:05:50 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,40 +1,37 @@
-import express from 'express';
-import { login, register, forgotPassword, verifyResetToken, resetPassword } from '../controllers/authCTL.js';
-import passport from '../config/passport.js';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import {
+  login,
+  register,
+  forgotPassword,
+  verifyResetToken,
+  resetPassword,
+} from "../controllers/authCTL.js";
+import passport from "../config/passport.js";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
 
-router.get('/google',
-    passport.authenticate('google', { 
-        scope: ['profile', 'email'],
-        session: false 
-    })
-);
+// router.get(
+//   "/google",
+//   passport.authenticate("google", {
+//     scope: ["profile", "email"],
+//     session: false,
+//   })
+// );
 
-router.get('/google/callback',
-    passport.authenticate('google', { 
-        session: false,
-        failureRedirect: '/login'
-    }),
-    (req, res) => {
-        try {
-            const { token } = req.user;
-            console.log(req.user);
-            // Chuyển hướng về frontend với token
-            res.redirect(`${process.env.FRONTEND_URL}/oauth-callback?token=${token}`);
-        } catch (error) {
-            console.error('Google callback error:', error);
-            res.redirect(`${process.env.FRONTEND_URL}/login?error=auth_failed`);
-        }
-    }
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  })
 );
 
 // Routes cho quên mật khẩu
-router.post('/forgot-password', forgotPassword);
-router.get('/reset-password/:token', verifyResetToken);
-router.post('/reset-password/:token', resetPassword);
+router.post("/forgot-password", forgotPassword);
+router.get("/reset-password/:token", verifyResetToken);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
