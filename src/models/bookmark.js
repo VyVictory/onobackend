3d03a0d61810467c34ef6154431bc998439eaa3c
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-05-14 10:20:03 +0700

    Update bookmark.js

--- src/models/bookmark.js
+++ src/models/bookmark.js
@@ -4,7 +4,7 @@ import moment from 'mongoose-timestamp';
 const bookMarkSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    savedAt: { type: Date, default: () => moment().tz('Asia/Ho_Chi_Minh').toDate() }
+    // savedAt: { type: Date, default: () => moment().tz('Asia/Ho_Chi_Minh').toDate() }
 });
 
 bookMarkSchema.index({ user: 1, post: 1 }, { unique: true });
